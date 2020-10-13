741480f24f3ff105efd1c399616ed0e33feaed01
Huongto1505it <72616957+Huongto1505it@users.noreply.github.com>
2020-10-13 22:24:43 +0700

    day code

---
+++ Baithuchanhso1/Bai3.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baithuchanhso1
+{
+    class stack
+    {
+        private int top;
+        private int[] s;
+        public stack()
+        {
+            top = -1;
+            s = new int[20];
+        }
+        public stack(int n)
+        {
+            top = -1;
+            s = new int[n];
+        }
+        public bool empty()
+        {
+            if (top == -1)
+                return true;
+            else return false;
+        }
+        public bool full()
+        {
+            if (top == s.Length - 1)
+                return true;
+            else return false;
+        }
+        public void push(int x)
+        {
+            if (full()) Console.WriteLine("Stack day");
+            else s[++top] = x;
+        }
+        public int pop()
+        {
+            if (empty()) throw new Exception("Stack rong");
+            else
+            {
+                int x = s[top--];
+                return x;
+            }
+        }
+
+
+    }
+}
